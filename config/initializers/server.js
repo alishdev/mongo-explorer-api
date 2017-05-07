c624e8964cdc359a833c41d118933a5445737326
dev-pro <alisher@dev-pros-MBP.home>
2017-05-07 11:37:02 -0400

    refactoring server.js

--- config/initializers/server.js
+++ config/initializers/server.js
@@ -20,8 +20,114 @@ var start =  function(cb) {
   // Configure express 
   app = express();
   var port = process.env.PORT || config.get('http:port');
-  var swaggerHost = config.get('swagger:host');
 
+  setAppMiddleware(app);
+
+      // use middleware to read headers and get mongo db properties
+  app.use(function(req, res, next){
+      readHeadersHandler(req, res, next);
+  });
+
+    // set routes
+  logger.info('[SERVER] Initializing routes');
+  var routes = require('../../app/routes/');
+  app.use('/', routes);
+
+  // Error handler
+  app.use(function(err, req, res, next){
+    errorHandler(err, req, res, next);
+  });
+
+  // serve swagger
+  var swaggerHost = config.get('swagger:host');
+  var swaggerSpec = initSwagger(swaggerHost);
+  app.get('/swagger.json', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+
+    // listen
+  app.listen(port);
+  logger.info('[SERVER] Listening on port ' + port);
+  logger.info('[SERVER] Swagger host ' + swaggerHost);
+  
+    // return to callback function
+  if (cb) {
+    return cb();
+  }
+};
+
+/**
+ * Set essential middleware in express app
+ * @method
+ * @param {object} express app instance
+ * @return {void}
+ */
+function setAppMiddleware(app){
+  app.engine('handlebars', exphbs({defaultLayout: '../../app/views/layouts/main'}));
+  app.set('view engine', 'handlebars');
+  app.set('views', __dirname + '/../../app/views');
+
+  app.use(morgan('common'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json({type: '*/*'}));
+
+  // set CORS
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.use(express.static(path.join(__dirname, '../../app/public')));
+}
+
+/**
+ * Error handler
+ * @method
+ * @param {object} error
+ * @param {object} request
+ * @param {object} response
+ * @param {object} next
+ * @return 500 status in case of error
+ */
+function errorHandler(err, req, res, next){
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: (app.get('env') === 'development.js' ? err : {})
+    });
+    next(err);
+}
+
+/**
+ * Read every request header and build connection string from header parameters
+ * @method
+ * @param {object} error
+ * @param {object} request
+ * @param {object} response
+ * @param {object} next
+ * @return {void}
+ */
+function readHeadersHandler(req, res, next){
+    // read mongo server properties from the header
+    var parseConnectionString = require('../../app/helpers/request-helper');
+    if (!parseConnectionString.isAPICall(req))
+      next();
+    else{
+      parseConnectionString.parseConnectionString(req, logger);
+      next();
+    }
+}
+
+/**
+ * Initialize swagger UI
+ * @method
+ * @param {string} swaggerHost name of the server that hosts swagger doc 
+ * @return {object} swaggerJSDoc swagger spec
+ */
+function initSwagger(swaggerHost)
+{
   // swagger definition
   var swaggerDefinition = {
     info: {
@@ -45,68 +151,7 @@ var start =  function(cb) {
   };
 
   // initialize swagger-jsdoc
-  var swaggerSpec = swaggerJSDoc(options);
-
-
-  app.engine('handlebars', exphbs({defaultLayout: '../../app/views/layouts/main'}));
-  app.set('view engine', 'handlebars');
-  app.set('views', __dirname + '/../../app/views');
-
-  app.use(morgan('common'));
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use(bodyParser.json({type: '*/*'}));
-
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-      // use middleware to read headers and get mongo db properties
-  app.use(function(req, res, next){
-      // read mongo server properties from the header
-      // req.connectionString = {};
-      // req.connectionString.server = req.headers['mongo-server'] || 'localhost';
-      // req.connectionString.port = req.headers['mongo-server-port'] || 27017;
-      // logger.info(req.connectionString);
-      var parseConnectionString = require('../../app/helpers/request-helper');
-      if (!parseConnectionString.isAPICall(req))
-        next();
-      else{
-        parseConnectionString.parseConnectionString(req, logger);
-        next();
-      }
-  });
-
-  logger.info('[SERVER] Initializing routes');
-  app.use(express.static(path.join(__dirname, '../../app/public')));
-
-  var routes = require('../../app/routes/');
-  app.use('/', routes);
-
-  // Error handler
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: (app.get('env') === 'development.js' ? err : {})
-    });
-    next(err);
-  });
-
-  // serve swagger
-  app.get('/swagger.json', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-  });
-
-  app.listen(port);
-  logger.info('[SERVER] Listening on port ' + port);
-  logger.info('[SERVER] Swagger host ' + swaggerHost);
-  
-  if (cb) {
-    return cb();
-  }
-};
+  return swaggerJSDoc(options);
+}
 
 module.exports = start;
